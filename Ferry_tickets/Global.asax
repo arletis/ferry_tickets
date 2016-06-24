21b636b7fb41ad425bbd0103c08f17f9a83c34e0
Norlan <Norlan@DESKTOP-5HKHV63>
2016-06-24 00:49:23 -0500

    Agregar archivos de proyecto.

---
+++ Ferry_tickets/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ferry_tickets.MvcApplication" Language="C#" %>
---
+++ Ferry_tickets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ferry_tickets.Startup))]
+namespace Ferry_tickets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
